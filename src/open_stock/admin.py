426d586545a5e85ffcfb1456ca21baee89e572c2
JC onLine <gillot.jc@orange.fr>
2025-06-10 12:40:03 +0200

    build(models admin); Create Category, Location, Product Admin. Docs: Database diagram, add Product & Location relation;4

--- src/open_stock/admin.py
+++ src/open_stock/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Category, Product, Reason, Provider, Company, Location, Stock, Movement, ShoppingList
 
-# Register your models here.
+
+class CategoryInline(admin.StackedInline):
+    model = Category
+    extra = 1
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "category", "location", "count_mini_alarm", "description", ]
+    prepopulated_fields = {"slug": ("name",)}
+    # inlines = [CategoryInline]
+
+
+class ProductInline(admin.StackedInline):
+    model = Product
+    extra = 1
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductInline]
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'type']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductInline]
--- src/open_stock/models.py
+++ src/open_stock/models.py
@@ -4,42 +4,77 @@ from account.models import CustomUser
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Catégorie'
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    address = models.TextField(blank=True)
+    type = models.CharField(max_length=100, blank=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Localisation'
 
 
 class Product(models.Model):
-    sku = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    alarm_min_part = models.IntegerField(default=0)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products',
+                                 blank=True, null=True, verbose_name="Catégorie")
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='locations',
+                                 blank=True, null=True, verbose_name="Rangement")
+    count_mini_alarm = models.IntegerField(default=0, verbose_name="Alarme stock mini")
+    price = models.DecimalField(max_digits=10, decimal_places=2,
+                                default=0, null=True, verbose_name="Prix")
+    sku = models.CharField(max_length=50, blank=True)
+    description = models.TextField(blank=True)
     slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Produit'
 
 
 class Reason(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Raison'
+
 
 class Provider(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     favorite = models.BooleanField(default=False)
+    # slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Fournisseur'
 
 
 class Company(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    description = models.TextField()
-
-
-class Location(models.Model):
-    name = models.CharField(max_length=100)
-    address = models.TextField()
-    type = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
 
 
 class Stock(models.Model):
@@ -50,6 +85,12 @@ class Stock(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     slug = models.SlugField(unique=True)
 
+    def __str__(self):
+        return self.product
+
+    class Meta:
+        verbose_name = 'Stockage'
+
 
 class Movement(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -57,12 +98,25 @@ class Movement(models.Model):
     location_dest = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="destination")
     provider_src = models.ForeignKey(Provider, on_delete=models.CASCADE, related_name="provider")
 
+    def __str__(self):
+        return self.product
+
+    class Meta:
+        verbose_name = 'Mouvement'
+
 
 class ShoppingList(models.Model):
+    created_date = models.DateTimeField(auto_now=True)
     provider_src = models.ForeignKey(Provider, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(null=True, blank=True)
     validate = models.BooleanField(default=False)
     location_dest = models.ForeignKey(Location, on_delete=models.CASCADE)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f"{self.created_date} par {self.user}"
+
+    class Meta:
+        verbose_name = 'Liste de course'
