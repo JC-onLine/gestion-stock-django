714572994e27856a683fb0a1c3dd49f51ac5eb24
JC onLine <gillot.jc@orange.fr>
2025-06-13 09:37:21 +0200

    build(models admin); Add  Company, Provider, Stock, Reason, Movement Admin.;4

--- src/open_stock/admin.py
+++ src/open_stock/admin.py
@@ -2,10 +2,39 @@ from django.contrib import admin
 from .models import Category, Product, Reason, Provider, Company, Location, Stock, Movement, ShoppingList
 
 
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ["name", "user", "description", ]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['company', 'name', 'address', 'type']
+    prepopulated_fields = {'slug': ('name',)}
+    # inlines = [CompanyInline]
+
+
 class CategoryInline(admin.StackedInline):
     model = Category
     extra = 1
     prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ["name", "favorite", "description", ]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ["company", "location", "product", "quantity", "user", "slug", ]
+    company_pk = "company"
+    location_pk = "location"
+    # company_name = Company.objects.get(pk=int(company_pk))
+    # location_name = Location.objects.get(pk=3)
+    prepopulated_fields = {"slug": ("location",)}
 
 
 @admin.register(Product)
@@ -28,8 +57,15 @@ class CategoryAdmin(admin.ModelAdmin):
     inlines = [ProductInline]
 
 
-@admin.register(Location)
-class LocationAdmin(admin.ModelAdmin):
-    list_display = ['name', 'address', 'type']
-    prepopulated_fields = {'slug': ('name',)}
-    inlines = [ProductInline]
+@admin.register(Reason)
+class ReasonAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    # prepopulated_fields = {'slug': ('name',)}
+    # inlines = [ProductInline]
+
+
+@admin.register(Movement)
+class MovementAdmin(admin.ModelAdmin):
+    list_display = ['product', 'location_src', 'provider_src']
+    # prepopulated_fields = {'slug': ('name',)}
+    # inlines = [ProductInline]
--- src/open_stock/models.py
+++ src/open_stock/models.py
@@ -2,19 +2,21 @@ from django.db import models
 from account.models import CustomUser
 
 
-class Category(models.Model):
+class Company(models.Model):
     name = models.CharField(max_length=100)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
-    slug = models.SlugField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, default="")
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Catégorie'
+        verbose_name = 'Compagnie'
 
 
 class Location(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=100, blank=True)
     address = models.TextField(blank=True)
     type = models.CharField(max_length=100, blank=True)
@@ -25,6 +27,18 @@ class Location(models.Model):
 
     class Meta:
         verbose_name = 'Localisation'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Catégorie'
 
 
 class Product(models.Model):
@@ -47,22 +61,11 @@ class Product(models.Model):
         verbose_name = 'Produit'
 
 
-class Reason(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Raison'
-
-
 class Provider(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     favorite = models.BooleanField(default=False)
-    # slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, default="")
 
     def __str__(self):
         return self.name
@@ -71,32 +74,43 @@ class Provider(models.Model):
         verbose_name = 'Fournisseur'
 
 
-class Company(models.Model):
-    name = models.CharField(max_length=100)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    description = models.TextField(blank=True)
-
-
 class Stock(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     slug = models.SlugField(unique=True)
 
-    def __str__(self):
-        return self.product
+    # def __str__(self):
+    #     return self.product
 
     class Meta:
         verbose_name = 'Stockage'
 
 
+class Reason(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    slug = models.SlugField(unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Raison'
+
+
 class Movement(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    location_src = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="source")
-    location_dest = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="destination")
-    provider_src = models.ForeignKey(Provider, on_delete=models.CASCADE, related_name="provider")
+    location_src = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="sources")
+    location_dest = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="destinations")
+    provider_src = models.ForeignKey(Provider, on_delete=models.CASCADE, related_name="providers")
+    reason = models.ForeignKey(Reason, on_delete=models.CASCADE, related_name="reasons", null=True, blank=True)
+    quantity = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True)
 
     def __str__(self):
         return self.product
